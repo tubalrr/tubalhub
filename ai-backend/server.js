1aaf9c795de77730921b7128e21344e59028a00d
tubalrr <tubalrr@gmail.com>
2026-09-20 08:44:21 +0800

    Update server.js

--- ai-backend/server.js
+++ ai-backend/server.js
@@ -110,7 +110,7 @@ function isAllowedReplicateUrl(value) {
 }
 
 const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_TOKEN = r8_9mkXj0u1irjIk8OUOpT1OlVNHwQenCW4ALQLW
+  auth: process.env.REPLICATE_API_TOKEN = r8_Inn**********************************
 });
 
 app.get("/", (_req, res) => {
