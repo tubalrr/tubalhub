48b5b3d2f10702bed4bd6f50cde501abd3bcc602
tubalrr <tubalrr@gmail.com>
2026-09-23 10:21:47 +0800

    Add Messenger launcher activity

---
+++ android-messenger/app/src/main/java/com/tubalhub/messenger/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.tubalhub.messenger
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+        setContentView(R.layout.activity_main)
+    }
+}
